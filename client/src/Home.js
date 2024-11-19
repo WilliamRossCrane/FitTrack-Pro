206af096039063a7c4e9fe005afbfaaf037c65e2
William Crane <william.crane@griffithuni.edu.au>
2024-11-19 19:03:05 +1000

    add landing page to client
    add .gitignore files for client and server

---
+++ client/src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+    <div>
+        <h1>Welcome to FitTrack Pro</h1>
+        <p>Your fitness journey starts here.</p>
+    </div>
+);
+
+export default Home;
