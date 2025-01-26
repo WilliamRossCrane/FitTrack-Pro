8ddeb7e0a7d94e572bb29503ff93e92f6bbe18ba
William Crane <wcran7@gmail.com>
2025-01-26 17:45:52 +1000

    Navbar - Component created with Initial design and look for the home page and exercises.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
-import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ExerciseDetail from './components/ExerciseDetail';
 
 const App = () => {
     return (
