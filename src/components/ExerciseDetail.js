c14ae36ce77ab996efeaab4542471cb07e941eb6
William Crane <68636614+WilliamRossCrane@users.noreply.github.com>
2025-02-08 13:20:25 +1000

    Finalised Exercise Details Page

--- src/components/ExerciseDetail.js
+++ src/components/ExerciseDetail.js
@@ -8,9 +8,23 @@ import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+    
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+    }
+    fetchExerciseData();
+  }, [id]);
+
   return (
     <Box>
-      <Detail />
+      <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos />
       <SimilarExercises />
     </Box>
