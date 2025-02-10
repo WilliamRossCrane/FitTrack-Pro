4dee8e1a33657792e47ca60d6cbd81b95021c57f
William Crane <wcran7@gmail.com>
2025-02-10 11:20:03 +1000

    Fixed API Calls

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -36,7 +36,6 @@ const Exercises = ({ exercises, setExercises, bodyPart}) => {
     window.scrollTo({ top: 1800, behavior: 'smooth'})
   }
 
-  useEffect
   return (
     <Box id="exercises"
     sx={{mt: { lg: '110px' }}}
